743a57c45552e05c82e13f78bf43cb4a00d2df64
Divyarani K <divyaranidhkls@gmail.com>
2025-05-11 09:10:09 +0530

    Refactor betterAuthServerClient configuration for clarity and structure

--- src/integrations/better-auth/index.ts
+++ src/integrations/better-auth/index.ts
@@ -1,78 +1,78 @@
-// // serverUrl
-// // webClientUrl
-
-// import { betterAuth } from "better-auth";
-// import { betterAuthSecret, serverUrl, webClientUrl } from "../../environment";
-// import { prismaAdapter } from "better-auth/adapters/prisma";
-// import { username } from "better-auth/plugins";
-// import { prismaClient } from "../prisma";
-
-// const betterAuthServerClient = betterAuth({
-//   baseURL: serverUrl,
-//   trustedOrigins: [webClientUrl],
-//   secret: betterAuthSecret,
-//   database: prismaAdapter(prismaClient, {
-//     provider: "postgresql",
-//   }),
-//   user: {
-//     modelName: "User",
-//   },
-//   session: {
-//     modelName: "Session",
-//   },
-//   account: {
-//     modelName: "Account",
-//   },
-//   verification: {
-//     modelName: "Verification",
-//   },
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-//   plugins: [username()],
-//   advanced: {
-//     defaultCookieAttributes: {
-//       sameSite: "none",
-//       secure: true,
-//       partitioned: true,
-//     },
-//   },
-// });
-
-// export default betterAuthServerClient;
-
-
-
+// serverUrl
+// webClientUrl
 
 import { betterAuth } from "better-auth";
+import { betterAuthSecret, serverUrl, webClientUrl } from "../../environment";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { username } from "better-auth/plugins";
 import { prismaClient } from "../prisma";
-import { serverUrl, webClientUrl } from "../../environment"
 
-export const betterAuthClient = betterAuth({
+const betterAuthServerClient = betterAuth({
   baseURL: serverUrl,
-  basePath: "/authentications",
+  trustedOrigins: [webClientUrl],
+  secret: betterAuthSecret,
   database: prismaAdapter(prismaClient, {
     provider: "postgresql",
   }),
-  trustedOrigins: [serverUrl, webClientUrl],
-  emailAndPassword: {
-    enabled: true,
-  },
   user: {
     modelName: "User",
+  },
+  session: {
+    modelName: "Session",
   },
   account: {
     modelName: "Account",
   },
-  session: {
-    modelName: "Session",
-    cookieCache: {
-      enabled: true,
-      maxAge: 15 * 60,
-    },
-  },
   verification: {
     modelName: "Verification",
   },
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [username()],
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      partitioned: true,
+    },
+  },
 });
+
+export default betterAuthServerClient;
+
+
+
+
+// import { betterAuth } from "better-auth";
+// import { prismaAdapter } from "better-auth/adapters/prisma";
+// import { prismaClient } from "../prisma";
+// import { serverUrl, webClientUrl } from "../../environment"
+
+// export const betterAuthClient = betterAuth({
+//   baseURL: serverUrl,
+//   basePath: "/authentications",
+//   database: prismaAdapter(prismaClient, {
+//     provider: "postgresql",
+//   }),
+//   trustedOrigins: [serverUrl, webClientUrl],
+//   emailAndPassword: {
+//     enabled: true,
+//   },
+//   user: {
+//     modelName: "User",
+//   },
+//   account: {
+//     modelName: "Account",
+//   },
+//   session: {
+//     modelName: "Session",
+//     cookieCache: {
+//       enabled: true,
+//       maxAge: 15 * 60,
+//     },
+//   },
+//   verification: {
+//     modelName: "Verification",
+//   },
+// });
